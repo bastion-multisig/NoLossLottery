3bd169d150589b51f2e93344c568c029ca4e90dd
strange-dv <andrii.dovzhenko.dv@gmail.com>
2022-03-09 17:43:30 +0200

    migrated to devnet

--- user-deposit/tests/user-deposit.ts
+++ user-deposit/tests/user-deposit.ts
@@ -1,66 +1,24 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import {Program, web3} from '@project-serum/anchor';
 import { UserDeposit } from '../target/types/user_deposit';
 import { clusterApiUrl, Connection, Keypair, Transaction, SystemProgram } from "@solana/web3.js";
 import { Token, TOKEN_PROGRAM_ID, MintLayout, AccountLayout } from "@solana/spl-token";
+import * as assert from "assert";
 
 describe('User Deposit', () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.UserDeposit as Program<UserDeposit>;
 
-  let mint;
-  let sender_token;
+  let mint_public_key = new web3.PublicKey(
+      "2cdTq4yUsMLXYSduo4QMd7nrjHzoCLXQsFxWvrj78TSy");
+  let sender_token_public_key = new web3.PublicKey(
+      "H32AhNTFN82tikKUK1YdfcY7gXc4PGHsraafPwhnHj9j"
+  )
   let receiver;
   let receiver_token;
 
-  it('Setup mints and token accounts', async () => {
-    mint = Keypair.generate();
-
-    let create_mint_tx = new Transaction().add(
-        // create mint account
-        SystemProgram.createAccount({
-          fromPubkey: program.provider.wallet.publicKey,
-          newAccountPubkey: mint.publicKey,
-          space: MintLayout.span,
-          lamports: await Token.getMinBalanceRentForExemptMint(program.provider.connection),
-          programId: TOKEN_PROGRAM_ID,
-        }),
-        // init mint account
-        Token.createInitMintInstruction(
-            TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
-            mint.publicKey, // mint pubkey
-            0, // decimals
-            program.provider.wallet.publicKey, // mint authority
-            program.provider.wallet.publicKey // freeze authority (if you don't need it, you can set `null`)
-        )
-    );
-
-    await program.provider.send(create_mint_tx, [mint]);
-
-    // more test here
-
-    sender_token = Keypair.generate();
-    let create_sender_token_tx = new Transaction().add(
-        // create token account
-        SystemProgram.createAccount({
-          fromPubkey: program.provider.wallet.publicKey,
-          newAccountPubkey: sender_token.publicKey,
-          space: AccountLayout.span,
-          lamports: await Token.getMinBalanceRentForExemptAccount(program.provider.connection),
-          programId: TOKEN_PROGRAM_ID,
-        }),
-        // init mint account
-        Token.createInitAccountInstruction(
-            TOKEN_PROGRAM_ID, // always TOKEN_PROGRAM_ID
-            mint.publicKey, // mint
-            sender_token.publicKey, // token account
-            program.provider.wallet.publicKey // owner of token account
-        )
-    );
-
-    await program.provider.send(create_sender_token_tx, [sender_token]);
-
+  it("Create receiver", async () => {
     receiver = Keypair.generate();
     receiver_token = Keypair.generate();
     let create_receiver_token_tx = new Transaction().add(
@@ -75,7 +33,7 @@ describe('User Deposit', () => {
         // init mint account
         Token.createInitAccountInstruction(
             TOKEN_PROGRAM_ID,
-            mint.publicKey, // mint
+            mint_public_key, // mint
             receiver_token.publicKey, // token account
             receiver.publicKey // owner of token account
         )
@@ -86,8 +44,8 @@ describe('User Deposit', () => {
     let mint_tokens_tx = new Transaction().add(
         Token.createMintToInstruction(
             TOKEN_PROGRAM_ID,
-            mint.publicKey, // mint
-            sender_token.publicKey, // receiver (should be a token account)
+            mint_public_key, // mint
+            sender_token_public_key, // receiver (should be a token account)
             program.provider.wallet.publicKey, // mint authority
             [],
             100
@@ -96,7 +54,7 @@ describe('User Deposit', () => {
 
     await program.provider.send(mint_tokens_tx);
 
-    console.log("token balance: ", await program.provider.connection.getTokenAccountBalance(sender_token.publicKey));
+    console.log("token balance: ", await program.provider.connection.getTokenAccountBalance(sender_token_public_key));
   });
 
   it('Deposit and get tokens', async () => {
@@ -104,13 +62,13 @@ describe('User Deposit', () => {
     await program.rpc.deposit(amount, {
       accounts: {
         sender: program.provider.wallet.publicKey,
-        senderToken: sender_token.publicKey,
+        senderToken: sender_token_public_key,
         receiverToken: receiver_token.publicKey,
-        mint: mint.publicKey,
+        mint: mint_public_key,
         tokenProgram: TOKEN_PROGRAM_ID,
       }
     })
-    console.log("sender token balance: ", await program.provider.connection.getTokenAccountBalance(sender_token.publicKey));
+    console.log("sender token balance: ", await program.provider.connection.getTokenAccountBalance(sender_token_public_key));
     console.log("receiver token balance: ", await program.provider.connection.getTokenAccountBalance(receiver_token.publicKey));
 
   })
